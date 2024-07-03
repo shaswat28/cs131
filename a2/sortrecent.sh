14376dbe2d698f92ee7891b238fc467cb67ed85c
shaswat28 <33365072+shaswat28@users.noreply.github.com>
2024-07-02 19:15:45 -0700

    Update sortrecent.sh

--- a2/sortrecent.sh
+++ a2/sortrecent.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-# Prompt user for the number of recent commands to sort
 read -p "Enter the number of recent commands to sort: " num
 
-# Validate if input is a number
 if ! [[ "$num" =~ ^[0-9]+$ ]]; then
     echo "Error: Please enter a valid number."
     exit 1
 fi
 
-# Get the list of recent commands
 history_list=$(tail -n "$num" ~/.bash_history)
 
-# Sort the recent commands
 sorted_commands=$(echo "$history_list" | sort)
 
-# Print the sorted commands
 echo "Recent commands sorted:"
 echo "$sorted_commands"
